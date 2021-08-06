d06e607b71c7e93002610a4b5136bdc84529a462
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-08-06 17:30:13 -0300

    Ready to deploy

--- src/app.ts
+++ src/app.ts
@@ -19,33 +19,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.sendStatus(500);
 });
 
-import axios from "axios";
-import { getRepository } from "typeorm";
-import Pokemon from "./entities/Pokemons";
-
-app.get("/populate", async (req,res)=>{
-  console.log("Entrou")
-  for(let i = 1; i <= 898; i ++){
-    const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-    const newPokemon = {
-      name: result.data.name,
-      number: result.data.id,
-      image: result.data.sprites.front_default,
-      weight: result.data.weight,
-      height: result.data.height,
-      baseExp: result.data.base_experience,
-      description: ""
-    }
-    const speciesResult = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
-    for (let j = 0; j < speciesResult.data.flavor_text_entries.length; j++) {
-      if(speciesResult.data.flavor_text_entries[j].language.name === "en"){
-        newPokemon.description =  speciesResult.data.flavor_text_entries[j].flavor_text.split("\n").join(" ")
-      }
-    }
-    await getRepository(Pokemon).insert(newPokemon);
-  }
-  res.send("OK");
-})
 
 app.post("/sign-up", userController.signUp);
 app.post("/sign-in", userController.singIn);
